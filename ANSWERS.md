3879926323335807579a46ad967be6cb35506d56
Kyle Pace <kpace2296@gmail.com>
2020-01-24 20:38:51 -0600

    changing yarn

--- ANSWERS.md
+++ ANSWERS.md
@@ -1,4 +1,21 @@
 - [ ] Why would you use class component over function components (removing hooks from the question)?
+
+    Class components are older but they were used for a very long time. Hooks are still very new so any old code you work with will likely use classes. Very important to be able to use classes.
+
+
 - [ ] Name three lifecycle methods and their purposes.
+
+    ComponentDidMount() runs as soon as the component is rendered. Used like the useEffect hook when you want code to run when the component renders.
+
+    Render() which is the code itself actually running. Not necessary when using hooks.
+
+    ComponentDidUpdate() This happens when react ‘reacts’ to something happening in the code and updates the said code.
+
+
 - [ ] What is the purpose of a custom hook?
+
+    Custom hooks are created so that you can reuse logic in your code.
+
 - [ ] Why is it important to test our apps?
+
+    Testing saves a lot of potential headache later. You don’t want to assume your code is working then find out two hours of coding later that something was broken all along. It’s a long term investment as lambda says.
--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
 
+  state = {
+    players: [],
+    playerName: [],
+    playerCountry: [],
+    searches: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:5000/api/players')
+    .then(res => res.json())
+    .then(players => {
+      console.log(players)
+      this.setState({ ...this.state, players: players})
+    })
+    .catch(err => console.log(err))
+  }
+
+  render() {
+    return(
+    <div className="App">
+      <div className='header'>
+        <h1>Players</h1>
+        <div className='player-cards'>
+          {this.state.players.map(player => (
+            <h2 key={player.id}>{player.name}</h2>
+          ))}
+        </div>
+      </div>
+    </div>
+  )}
+}
+    
 export default App;
